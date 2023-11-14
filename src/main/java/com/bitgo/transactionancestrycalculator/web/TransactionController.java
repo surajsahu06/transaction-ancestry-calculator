a558708ee216b9594272c1744cdc23e640ab5365
surajsahu06 <surajsahu06@gmail.com>
2023-11-14 05:09:52 -0700

    updated the response

--- src/main/java/com/bitgo/transactionancestrycalculator/web/TransactionController.java
+++ src/main/java/com/bitgo/transactionancestrycalculator/web/TransactionController.java
@@ -5,12 +5,14 @@ import com.bitgo.transactionancestrycalculator.dto.Transaction;
 import com.bitgo.transactionancestrycalculator.service.AncestryCalculator;
 import com.bitgo.transactionancestrycalculator.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 public class TransactionController {
@@ -27,7 +29,7 @@ public class TransactionController {
     }
 
     @GetMapping(value = "/print-tx-details")
-    public void printTransactions() {
+    public ResponseEntity<List<Transaction>> printTransactions() {
         String blockHash;
         List<Root> transactions = new ArrayList<>();
         try {
@@ -40,5 +42,6 @@ public class TransactionController {
         Map<String, List<String>> ancestorsMap = ancestryCalculator.findAllAncestors(inputMap);
         List<Transaction> res = ancestryCalculator.getLargestAncestors(ancestorsMap, 10);
         res.forEach(System.out::println);
+        return ResponseEntity.ok(res);
     }
 }
